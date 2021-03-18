3b6b9a10267ce741da4555c3b4c358729e00b483
berrybab6 <bedatuassefa@gmail.com>
2021-03-18 15:45:29 +0300

    Department Feat created

--- EdTech/settings.py
+++ EdTech/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Student.apps.StudentConfig'
+    'Student.apps.StudentConfig',
+    "users.apps.UsersConfig",
+    "department.apps.DepartmentConfig"
 ]
 
 MIDDLEWARE = [
