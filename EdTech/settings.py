3bb713ae5dfe167fdceb3c97fe9b9a2e2f106a0d
berrybab6 <bedatuassefa@gmail.com>
2021-03-18 16:20:04 +0300

    Resource Model Created

--- EdTech/settings.py
+++ EdTech/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Student.apps.StudentConfig',
-    "users.apps.UsersConfig"
+    "users.apps.UsersConfig",
+    "resources.apps.ResourcesConfig"
 ]
 
 MIDDLEWARE = [
--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Resources(models.Model):
+    EXAMS = 1
+    ASSIGNMENT = 2
+    TUTORIALS = 3
+    BOOKS = 4
+    LECTURES = 5
+
+    RESOURCE_TYPES = (
+        (EXAMS, 'exams'),
+        (BOOKS, 'books'),
+        (LECTURES, 'lectures'),
+        (TUTORIALS, 'tutorials'),
+        (ASSIGNMENT, 'assignment'),
+    )
+
+    SOFTWARE = 1
+    MECHANICAL = 2
+    CIVIL = 3
+    CHEMICAL = 4
+    ELECTRICAL =5 
+    IT = 6
+    BIOMED = 7
+    CATEGORY = (
+        (SOFTWARE, "software"),
+        (MECHANICAL, "mechanical"),
+        (CIVIL, "civil"),
+        (CHEMICAL, "chemical"),
+        (ELECTRICAL, "electrical"),
+        (IT, "it"),
+        (BIOMED, "biomed")
+    )
+    resource_type = models.PositiveSmallIntegerField(choices=RESOURCE_TYPES)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    category = models.PositiveSmallIntegerField(choices=CATEGORY)
