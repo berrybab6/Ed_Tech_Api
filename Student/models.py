a84f421fb9f80e9b4226da63fa97d8e50cde74c8
berrybab6 <bedatuassefa@gmail.com>
2021-03-18 15:49:55 +0300

    Department Model Created

--- Student/models.py
+++ Student/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField(blank=True, null=True, max_length=150)
+    head = models.CharField(blank=True, null=True, max_length=150)
